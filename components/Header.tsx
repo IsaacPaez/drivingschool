f1c4f3da47cf1296742aeac3e7d20db5f3667400
Botopia-SAS <botopia.sas@gmail.com>
2025-03-06 18:15:00 -0500

    Solucionanod errores

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
