b4257e5138943fec8f4d5fe26fdfbac7d162d327
ANDREYPLAZAST <asptstevenplazas@gmail.com>
2025-05-24 12:48:16 -0500

    solcuion types

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "s.gravatar.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.auth0.com",
+      },
     ],
   },
 
