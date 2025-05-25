9de72f349745b5407d17f3b0c06dacb8c0af7450
ANDREYPLAZAST <asptstevenplazas@gmail.com>
2025-05-24 21:52:09 -0500

    a

--- components/StudentsCalendar/StudentBookedClasses.tsx
+++ components/StudentsCalendar/StudentBookedClasses.tsx
@@ -18,7 +18,7 @@ const StudentBookedClasses: React.FC<Props> = ({ ticketClasses }) => (
       <li key={tc._id} className="py-3 flex flex-col md:flex-row md:items-center md:justify-between">
         <span className="font-semibold text-[#0056b3]">{tc.type}</span>
         <span className="text-xs text-gray-500">{new Date(tc.date).toLocaleDateString()} {tc.hour}</span>
-        <span className="text-xs text-gray-500">Duration: {tc.duration}</span>
+        <span className="text-xs text-gray-500">Duraation: {tc.duration}</span>
       </li>
     ))}
     {ticketClasses.length === 0 && <li className="text-gray-400 py-4">No booked classes</li>}
